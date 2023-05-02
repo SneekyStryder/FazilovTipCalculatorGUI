ef279ab3b8ae6c5906d810c4799f3e5928a827c6
Imran Fazilov <ifazilov3584@bths.edu>
2023-05-02 12:25:24 -0400

    period end

--- src/TipCalculatorGUI.java
+++ src/TipCalculatorGUI.java
@@ -1,2 +1,38 @@
-package PACKAGE_NAME;public class TipCalculatorGUI {
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class TipCalculatorGUI extends JFrame implements ActionListener {
+    private JPanel mainPanel;
+    private JTextField billTextField;
+    private JTextField tipTextField;
+    private JTextField NumPeopleTextField;
+    private JButton TipMinusButton;
+    private JButton NumPeopleMinusButton;
+    private JTextArea TipCostTextArea;
+    private JTextArea TotalCostTextArea;
+    private JButton NumPeoplePlusButton;
+    private JButton TipPlusButton;
+
+    public TipCalculatorGUI() {
+        createUIComponents();
+    }
+
+    private void createUIComponents() {
+        setContentPane(mainPanel);
+        setTitle("Tip Calculator");
+        setSize(300, 400);
+        setLocation(450, 100);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        TipMinusButton.addActionListener(this);
+        TipPlusButton.addActionListener(this);
+        NumPeopleMinusButton.addActionListener(this);
+        NumPeoplePlusButton.addActionListener(this);
+        setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
 }
