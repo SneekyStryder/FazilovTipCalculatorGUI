cc3000b63b4a4815813dd2538b0d1e2afbbc52f2
Sneeky Stryder <ifazilov3584@bths.edu>
2023-06-08 12:59:43 -0400

    done

--- src/TipCalculatorGUI.java
+++ src/TipCalculatorGUI.java
@@ -13,6 +13,9 @@ public class TipCalculatorGUI extends JFrame implements ActionListener {
     private JTextArea TotalCostTextArea;
     private JButton NumPeoplePlusButton;
     private JButton TipPlusButton;
+    private double bill;
+    private double tipPercent;
+    private int numPeople;
 
     public TipCalculatorGUI() {
         createUIComponents();
@@ -24,11 +27,66 @@ public class TipCalculatorGUI extends JFrame implements ActionListener {
         setSize(300, 400);
         setLocation(450, 100);
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        TipMinusButton.addActionListener(this);
-        TipPlusButton.addActionListener(this);
-        NumPeopleMinusButton.addActionListener(this);
-        NumPeoplePlusButton.addActionListener(this);
+        TipMinusButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                int percent = Integer.parseInt(tipTextField.getText());
+                percent -= 1;
+                tipTextField.setText("" + percent);
+                calculate();
+            }
+        });
+        TipPlusButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                int percent = Integer.parseInt(tipTextField.getText());
+                percent += 1;
+                tipTextField.setText("" + percent);
+                calculate();
+            }
+        });
+        NumPeopleMinusButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                int numPeople = Integer.parseInt(NumPeopleTextField.getText());
+                numPeople -= 1;
+                NumPeopleTextField.setText("" + numPeople);
+                calculate();
+            }
+        });
+        NumPeoplePlusButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                int numPeople = Integer.parseInt(NumPeopleTextField.getText());
+                numPeople += 1;
+                NumPeopleTextField.setText("" + numPeople);
+                calculate();
+            }
+        });
         setVisible(true);
+    }
+
+    private void calculate() {
+        getBill();
+        getTipPercent();
+        getNumPeople();
+        double tipTotal = bill * tipPercent;
+        double tipPer = tipTotal / numPeople;
+        TipCostTextArea.setText("" + tipPer);
+        double costPer = bill / numPeople + tipPer;
+        TotalCostTextArea.setText("" + costPer);
+    }
+
+    private void getBill() {
+        bill = Double.parseDouble(billTextField.getText());
+    }
+
+    private void getTipPercent() {
+        tipPercent = ((double) Integer.parseInt(tipTextField.getText())) / 100;
+    }
+
+    private void getNumPeople() {
+        numPeople = Integer.parseInt(NumPeopleTextField.getText());
     }
 
     @Override
